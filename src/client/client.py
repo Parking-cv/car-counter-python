78ff64ae7b8d0c92a33dd8682067050b9c423bda
Dustin-PC\Dustin <1998davery2013@gmail.com>
2020-02-11 05:23:50 -0600

    Getting the image checking to work within reasonable time, still way too slow will need to optimize somehow.(Tested with personal webcam, have not tested on Pi yet)

--- src/client/client.py
+++ src/client/client.py
@@ -2,6 +2,7 @@
 # python client.py -s SERVER_IP
 
 from imutils.video import VideoStream
+import numpy as np
 import imagezmq
 from PIL import Image
 from datetime import datetime
@@ -11,44 +12,52 @@ import time
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-s", "--server-ip", required=True,
+ap.add_argument("-s", "--server-ip", default='localhost',
                 help="ip address of the server to which the client will connect")
 ap.add_argument("-rw", "--res-width", type=int, default=272,
                 help="resolution width of captured footage")
 ap.add_argument("-rh", "--res-height", type=int, default=208,
                 help="resolution height of captured footage")
-ap.add_argument("-f", "--framerate", type=int, default=80,
+ap.add_argument("-f", "--framerate", type=int, default=20,
                 help="framerate of captured footage")
 args = vars(ap.parse_args())
 
-def check_image(frame):
+def check_image(frame, skippedPixels=3):
     if globals()['lastFrame'] is None:
         globals()['lastFrame'] = frame
     else:
         print("Start Time: " + datetime.now().strftime("%M:%S.%f"))
 
-        i1 = Image.open("50qualTestImage.jpg")
-        i2 = Image.open("100qualTestImage.jpg")
-        assert i1.mode == i2.mode, "Different kinds of images."
+        i1 = globals()['lastFrame']
+        i2 = frame
+        assert i1.shape == i2.shape, "Different kinds of images."
         assert i1.size == i2.size, "Different sizes."
 
-        pairs = zip(i1.getdata(), i2.getdata())
-        if len(i1.getbands()) == 1:
-            dif = sum(abs(p1 - p2) for p1, p2 in pairs)
+        pairs = zip(i1[::skippedPixels, ::skippedPixels], i2[::skippedPixels, ::skippedPixels])
+        if len(i1[0]) == 1:
+            total = 0
+            for p1, p2 in pairs:
+                total += abs(p1-p2)
+            dif = total
         else:
-            dif = sum(abs(c1 - c2) for p1, p2 in pairs for c1, c2 in zip(p1, p2))
+            total = 0
+            diff = lambda a, b: abs(int(a)-int(b))
+            diff = np.vectorize(diff)
+            for p1, p2 in pairs:
+                for c1, c2 in zip(p1,p2):
+                    total += np.sum(diff(c1[:], c2[:]))
+            dif = total
 
-        ncomponents = i1.size[0] * i1.size[1] * 3
+        ncomponents = (i1.shape[0] * i1.shape[1] * 3)/5
         print("Difference (percentage):", (dif / 255.0 * 100) / ncomponents)
 
-        print("Start Time: " + datetime.now().strftime("%M:%S.%f"))
+        print("End Time: " + datetime.now().strftime("%M:%S.%f"))
         globals()['lastFrame'] = frame
 
 # initialize the ImageSender object with the socket address of the
 # server
-sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
-    args["server_ip"]))
-
+# sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
+#     args["server_ip"]))
 # get the host name and initialize the video stream
 rpiName = socket.gethostname()
 vs = VideoStream(usePiCamera=True, resolution=(args["res_width"],
@@ -62,4 +71,5 @@ globals()['lastFrame'] = None
 while True:
     # read the frame from the camera and send it to the server
     frame = vs.read()
-    sender.send_image(rpiName, frame)
+    check_image(frame,3)
+    # sender.send_image(rpiName, frame)

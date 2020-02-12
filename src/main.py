9d2e65640c1ac1b19cdcea38e84d38930d9714c3
David Wiles <davidwiles33@gmail.com>
2020-02-12 13:11:03 -0600

    example upload frame in network manager

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+from datetime import datetime
 from time import sleep
 
 from src.network.NetworkManager import NetworkManager
@@ -9,21 +9,13 @@ from src.network.NetworkManager import NetworkManager
 def main():
 
     # Test network
-    manager = NetworkManager("http://localhost:3000", {"frame": "/frames"})
-    rook = open("../img/white_rook.png", 'rb')
-    knight = open("../img/white_knight.png", 'rb')
-    bishop = open("../img/white_bishop.png", 'rb')
-    queen = open("../img/white_queen.png", 'rb')
-    king = open("../img/white_king.png", 'rb')
+    manager = NetworkManager("http://localhost:3000", {"frame": "/pi/frames"})
+    knight = open("img/black_knight.png", "rb")
 
     manager.start()
 
     manager.uploadFrames({
-        'rook': rook,
-        'knight': knight,
-        'bishop': bishop,
-        'queen': queen,
-        'king': king
+		str(datetime.utcnow().isoformat('T')) + 'Z' : knight
     })
 
     sleep(1)

5bd317521d7a3629a991770c7312469612de31a0
Dustin-PC\Dustin <1998davery2013@gmail.com>
2020-02-12 18:19:54 -0600

    Saving images and sending them based on a time delay. Need to work on deleting old images as well as sending them to the server.

--- src/client/client.py
+++ src/client/client.py
@@ -4,12 +4,12 @@
 from imutils.video import VideoStream
 from datetime import datetime
 import argparse
-import socket
 import time
 from skimage.metrics import structural_similarity
 import asyncio
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 import cv2
+import os
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -22,6 +22,8 @@ ap.add_argument("-rh", "--res-height", type=int, default=208,
 ap.add_argument("-f", "--framerate", type=int, default=20,
                 help="framerate of captured footage")
 args = vars(ap.parse_args())
+
+globals()["motion_tracking"] = True
 
 def check_image(previousFrame, frame):
     # print("Start Time: " + datetime.now().strftime("%M:%S.%f"))
@@ -58,38 +60,58 @@ globals()['lastFrame'] = None
 
 def saving_frames(frame, count):
     print("Saving something: " + str(count))
+    # TimeStamp
+    filename = "images/frame_" + datetime.now().isoformat() + ".jpg"
+    cv2.imwrite(filename, frame)
 
 def motion_track(previousFrame, frame, count, acceptable_difference):
+    # In case this frame is correct, get a correct datetime for the start time
+    start = datetime.now()
     if check_image(previousFrame, frame):
-        sendStoredFiles()
+        globals()["motion_tracking"] = False
+        time.sleep(5)
+        end = datetime.now()
+        sendStoredFiles(start, end)
+        globals()["motion_tracking"] = True
     else:
         print("No motion Detected: " + str(count))
 
-def sendStoredFiles():
-    print("Sending files")
+def sendStoredFiles(startTime, endTime):
+    sentFiles = dict()
+    currentItems = os.listdir("images")
+    for filename in currentItems:
+        timestamp = filename[filename.find("_")+1:filename.rfind(".")]
+        timeObj = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%f")
+        if startTime < timeObj < endTime:
+            sentFiles[timestamp] = filename
+    return sentFiles
+
 
 async def client():
     vs = VideoStream(usePiCamera=False, resolution=(args["res_width"],
                                                     args["res_height"]), framerate=args["framerate"]).start()
 
     loop = asyncio.get_event_loop()
-    executor = ProcessPoolExecutor(2)
+    # 2 Threads, 1 for motion tracking and 1 for saving, allocating more for tracker as it will probably be
+    # happening multiple times at once
+    trackerExecutor = ThreadPoolExecutor(2)
+    saveExecutor = ThreadPoolExecutor(1)
 
     lastFrame = None
     count = 0
     frameCount = 0
     while True:
         frame = vs.read()
-        asyncio.ensure_future(loop.run_in_executor(executor, saving_frames, frame, count))
+        asyncio.ensure_future(loop.run_in_executor(saveExecutor, saving_frames, frame, count))
         # Checking once every half second
         if frameCount == (args["framerate"]/2):
-            if lastFrame is not None:
-                asyncio.ensure_future(loop.run_in_executor(executor, motion_track, lastFrame, frame, count, .8))
+            if lastFrame is not None and globals()["motion_tracking"]:
+                asyncio.ensure_future(loop.run_in_executor(trackerExecutor, motion_track, lastFrame, frame, count, .8))
             lastFrame = frame
             frameCount = 0
         count += 1
         frameCount += 1
         time.sleep(1/args["framerate"])
-
+#Beign the client part of this
 loop = asyncio.get_event_loop()
 loop.run_until_complete(client())
